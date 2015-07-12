b135d886881198d0c6f454a972f6046bc899368c
Sethu Kettimuthu <sethu.kettimuthu@gmail.com>
2015-07-12 15:11:39 -0400

    minor correction

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ plot2<-function(){
         source<-file("household_power_consumption.txt")
         subset<-read.csv.sql("household_power_consumption.txt","select * from source where Date in ('1/2/2007', '2/2/2007')", header=TRUE, sep= ";")
         date_time<-strptime(paste(subset$Date, subset$Time, sep = " "),"%d/%m/%Y %H:%M:%S")
-        plot(date_time,Subset$Global_active_power,type='l',col="red",xlab="",ylab="Global Active Power(kilowatts)",main="power consumed by specified date and time")
+        plot(date_time,subset$Global_active_power,type='l',col="red",xlab="",ylab="Global Active Power(kilowatts)",main="power consumed by specified date and time")
         closeAllConnections()
         
 }
